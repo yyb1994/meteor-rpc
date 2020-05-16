4b8fd6490223167d2ee2b543a9dc312b14a17f87
Super <758458195@qq.com>
2020-05-16 22:42:43 +0800

    常量分类

--- meteor-common/src/main/java/com/meteor/common/core/CommonConstants.java
+++ meteor-common/src/main/java/com/meteor/common/core/CommonConstants.java
@@ -1,12 +1,19 @@
 package com.meteor.common.core;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 公共字典/常量
  * 接口定义常量与class 定义常量区别 https://www.cnblogs.com/EasonJim/p/7871753.html
+ *
  * @author SuperMu
  * @time 2019-07-06
  */
 public interface CommonConstants {
+
+    public static class Server {
+        public static ConcurrentHashMap<String, Class> REGISTER_SERVICE_MAP = new ConcurrentHashMap<>();
+    }
 
     String PROVIDER = "provider";
 
@@ -16,4 +23,6 @@ public interface CommonConstants {
     int DEFAULT_IO_THREADS = Math.min(Runtime.getRuntime().availableProcessors() + 1, 32);
 
     String HEARTBEAT_EVENT = null;
+
+
 }
